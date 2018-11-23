75ef764d4b03fec2aab550b09b0df2acea77fcad
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-23 14:47:03 -0500

    Create APlusB.cpp

---
+++ Meeting 6/Test Problems/APlusB.cpp
@@ -0,0 +1,14 @@
+// link : https://dmoj.ca/problem/aplusb
+#include <iostream>
+
+using namespace std;
+int n, a, b;
+int main()
+{
+   cin >> n;
+   while(n--){
+       cin>>a>>b; cout<<a+b<<endl;
+   }
+   
+   return 0;
+}
